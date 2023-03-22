11ee475c318beb67f9914118989febb61f1af952
Yani <yanixd14@gmail.com>
2023-03-22 12:58:01 +0100

    Base2: Comunicación cliente-servidorMeteo terminada

--- MeteoDataServer_grpc.py
+++ MeteoDataServer_grpc.py
@@ -8,11 +8,16 @@ from google.protobuf import empty_pb2
 class SensorDataService (SensorData_pb2_grpc.SensorDataServiceServicer):
 
     
+    # Method that recieves the MeteoData info
     def SendMeteoData(self, request, context):
         print("Received meteo data from sensor " + str(request.sensor_id))
         print("Temperature: " + str(request.temperature))
         print("Humidity: " + str(request.humidity))
         print("Timestamp: " + str(request.timestamp.ToDatetime()))
+        return empty_pb2.Empty()
+
+    # Process the meteo_data with meteo_utils.py
+    def ProcessMeteoData(self, request, context):
         meteo_data = SensorData_pb2.RawMeteoData()
         meteo_data.sensor_id = request.sensor_id
         meteo_data.temperature = request.temperature
@@ -20,8 +25,8 @@ class SensorDataService (SensorData_pb2_grpc.SensorDataServiceServicer):
         meteo_data.timestamp.FromDatetime(request.timestamp.ToDatetime())
         airwellness = meteo_utils.MeteoDataProcessor()
         airwellness_procesado = airwellness.process_meteo_data(meteo_data)
-        print(airwellness_procesado)
-        return empty_pb2.Empty()
+        print("Calculated air wellness index: " + str(airwellness_procesado))
+        return SensorData_pb2.AirWellnessCoefficient(airwellness=airwellness_procesado)
     
     def serve(self):
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
